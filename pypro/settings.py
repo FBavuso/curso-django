85c4ead8b879bbd3a4b5e147a3f55813c6a70bd2
Felipe Bavuso <felipebavuso@gmail.com>
2022-06-09 21:17:36 -0300

    configurando python-decouple ci default debug

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  'django-insecure-h*86xtcnk&f@nbr&%7b(ye%cubd&a&^)j5-=!^kot6zcbg)%70'
+SECRET_KEY = config('SECRET_KEY')#'django-insecure-h*86xtcnk&f@nbr&%7b(ye%cubd&a&^)j5-=!^kot6zcbg)%70'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast= bool)

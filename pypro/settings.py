698425c33032a0277f0d552fc6f1231628e50ac8
Felipe Bavuso <felipebavuso@gmail.com>
2022-07-31 18:09:17 -0300

    Configurando Collectfast

--- pypro/settings.py
+++ pypro/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'colectfast',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 ]
